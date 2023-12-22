7e959711cf42f28675f6741607f117d1859264ae
Matteo <degano83@yahoo.com>
2023-12-22 11:22:59 +0100

    Ajout commentaire comprehensible maintenant

--- BDD.py
+++ BDD.py
@@ -10,10 +10,11 @@ from but1 import *
 from but2 import *
 from but3 import *
 
+#connection a la base de donnée SQLITE
 with sqlite3.connect("SAE.db") as connection:
     cursor = connection.cursor()
 
-# suppretion des tables pour mettre a jour les données dedans si il y a des modifs
+# Suppretion des tables pour mettre a jour les données dedans si il y a des modifs
 cursor.execute(
    "DROP TABLE IF EXISTS ENSEIGNE")
 cursor.execute(
@@ -23,6 +24,7 @@ cursor.execute(
 cursor.execute(
    "DROP TABLE IF EXISTS BIBLE")
 
+# Création des tables
 cursor.execute(
    "CREATE TABLE IF NOT EXISTS PROF ("
        "cle_prof INTEGER PRIMARY KEY,"
@@ -63,18 +65,22 @@ cursor.execute(
        "NOMBRE_GROUPE INTEGER,"
        "BUT TEXT)")
 
-# récuperation des données depuis les import des fichiers disponible dans le dossier CriteresRessources
+# Récuperation des données depuis les import des fichiers disponible dans le dossier CriteresRessources
+# Fonction d'insertion des données:
 
-#fonction d'insertion des données
+# liste des colonnes de la table à inserer
 column_list_RESSOURCE = ['code_apogee', 'libelle', 'total_cm', 'total_td', 'total_tp', 'HETD', 'HETD_PACOME']
-
+# Fonction d'insertion qui ajoute les tuples disponible dans ligne_donnees (contient les données sous forme de ligne) et les insert 1 par 1, colonne par colonne.
 def ajouterDonneesToRESSOURCE(ligne_donnees,table_name):
     colonnes = ', '.join(column_list_RESSOURCE)
     valeurs = ', '.join(['?'] * len(column_list_RESSOURCE))
     cursor.execute(f"INSERT INTO {table_name} ({colonnes}) VALUES ({valeurs})", tuple(ligne_donnees))
 
+# Liste des Semestre (relié au fichier de recuperation  de donnée)
 liste_S1R1 = ['S1R101', 'S1R102', 'S1R103', 'S1R104', 'S1R105', 'S1R106', 'S1R107', 'S1R108', 'S1R109', 'S1R110', 'S1R111', 'S1R112']
 liste_S2R2 = ['S2R201', 'S2R202', 'S2R203', 'S2R204', 'S2R205', 'S2R206', 'S2R207', 'S2R208', 'S2R209', 'S2R210', 'S2R211', 'S2R212', 'S2R213', 'S2R214']
+
+# Fonction de remplissage pour éviter les répetitions de ligne
 def remplissageInRESSOURCE(numeroListeSemestre):
     for S1R1 in numeroListeSemestre:
         # Appelez la fonction avec les paramètres appropriés
@@ -83,10 +89,14 @@ def remplissageInRESSOURCE(numeroListeSemestre):
 remplissageInRESSOURCE(liste_S1R1)
 remplissageInRESSOURCE(liste_S2R2)
 
-#TODO: ajouter le NOMBRE_GROUPE et BUT dans la fonction de recuperation de donnée pour pouvoir la recuperer ici plus simplement
+# TODO: ajouter le NOMBRE_GROUPE et BUT dans la fonction de recuperation de donnée pour pouvoir la recuperer ici plus simplement
+# Code correspondant au TODO
+
+# Liste des colonnes de la tablea BIBLE
 column_list_BIBLE = ['code_apogee', 'libelle', 'total_cm', 'total_td', 'total_tp', 'HETD', 'HETD_PACOME', 'NOMBRE_GROUPE', 'BUT']
 
-def ajouterDonneesToENSEIGNE(ligne_donnees, GroupeTD, BUT,table_name):
+# Fonction d'insertion pareil que RESSOURCE
+def ajouterDonneesToBIBLE(ligne_donnees, GroupeTD, BUT,table_name):
     colonnes = ', '.join(column_list_BIBLE)
     valeurs = ', '.join(['?'] * len(column_list_BIBLE))
     ligne_donnees_complet = ligne_donnees.tolist()
@@ -95,15 +105,15 @@ def ajouterDonneesToENSEIGNE(ligne_donnees, GroupeTD, BUT,table_name):
     cursor.execute(f"INSERT INTO {table_name} ({colonnes}) VALUES ({valeurs})", tuple(ligne_donnees_complet))
 
 
-# Supposons que vous avez une liste de valeurs S1R1..
+# Fonction de remplissage pareil que RESSOURCE
 
-def remplissageInENSEIGNE(numeroListeSemestre,nombreGroupe,BUT):
+def remplissageInBIBLE(numeroListeSemestre,nombreGroupe,BUT):
     for S1R1 in numeroListeSemestre:
         # Appelez la fonction avec les paramètres appropriés
-        ajouterDonneesToENSEIGNE(globals()[S1R1], nombreGroupe, BUT, 'BIBLE')
+        ajouterDonneesToBIBLE(globals()[S1R1], nombreGroupe, BUT, 'BIBLE')
 
-remplissageInENSEIGNE(liste_S1R1,S1nbGroupeTD, 'BUT1')
-remplissageInENSEIGNE(liste_S2R2,S2nbGroupeTD, 'BUT1')
+remplissageInBIBLE(liste_S1R1,S1nbGroupeTD, 'BUT1')
+remplissageInBIBLE(liste_S2R2,S2nbGroupeTD, 'BUT1')
 
 connection.commit()
 connection.close()

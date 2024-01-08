faf171d07b16f526d35fd1ce36fb4c67949a8b23
RayanPIOCTMOUDNIP22011981 <rayan.pioct@etu.univ-amu.fr>
2024-01-08 12:02:07 +0100

    c'est faux mais me faut le code

--- QuiFaitQuoi/ExtractionQuiFaitQuoi.py
+++ QuiFaitQuoi/ExtractionQuiFaitQuoi.py
@@ -6,9 +6,22 @@ Le premier Intervenant renseigné dans chaque ressources est considéré par con
 """
 
 # On charge le classeur Excel
-QuiFaitQuoi = pd.ExcelFile('../Documents/QuiFaitQuoi_beta1.ods')
+QuiFaitQuoi = pd.ExcelFile('../Documents/QuiFaitQuoi_beta1.xlsx')
 
 # On établie la liste des feuilles qui nous intéressent
 ListeFeuillesQuiFaitQuoi = sorted(QuiFaitQuoi.sheet_names)
 
 print(ListeFeuillesQuiFaitQuoi)
+
+def RecuperationMatiere(FeuilleActuelle):
+    Index = QuiFaitQuoi["Intervenants"]
+    while couleurs != blanc and texte != null:
+        if couleurs change mais != blanc
+
+
+def RecuperationParFeuille(ListeFeuilles):
+    for Feuille in ListeFeuilles:
+        RecuperationMatiere(Feuille)
+    QuiFaitQuoi.close()
+
+RecuperationParFeuille(ListeFeuillesQuiFaitQuoi)

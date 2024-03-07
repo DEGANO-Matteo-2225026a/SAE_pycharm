2776dbc5c729ffdff05470994207d44bb176d60f
MailysGodinaudG21207635 <mailys.godinaud@etu.univ-amu.fr>
2024-03-07 22:40:38 +0100

    probleme reglé, fini

--- generationFicheProf.py
+++ generationFicheProf.py
@@ -55,15 +55,53 @@ class GenerationFicheProf:
                     continue
                 ligneH = trouver_ligne(feuille, "Matieres", 1)
                 feuille.insert_rows(ligneH + 1)
-                for i in range(6, 13):
-                    cellule_selectionnee = f.cell(row=horaires, column=i).value
-                    if cellule_selectionnee is None:
-                        cellule_selectionnee = f.cell(row=horaires, column=i+6).value
-                    feuille.cell(row=ligneH + 1, column=1, value=f.title)
-                    feuille.cell(row=ligneH + 1, column=i-3, value=cellule_selectionnee)
-                    # On insère l'année
-                    cellule_selectionnee = f.cell(row=horaires, column=3).value
-                    feuille.cell(row=ligneH + 1, column=2, value=cellule_selectionnee)
+
+                cm1 = f.cell(row=horaires, column=6).value
+                if cm1 is None:
+                    cm1 = 0
+                cm2 = f.cell(row=horaires, column=12).value
+                if cm2 is None:
+                    cm2 = 0
+                td1 = f.cell(row=horaires, column=7).value
+                if td1 is None :
+                    td1 = 0
+                td2 = f.cell(row=horaires, column=13).value
+                if td2 is None:
+                    td2 = 0
+                tpd1 = f.cell(row=horaires, column=8).value
+                if tpd1 is None:
+                    tpd1 = 0
+                tpd2 = f.cell(row=horaires, column=14).value
+                if tpd2 is None:
+                    tpd2 = 0
+                tp1 = f.cell(row=horaires, column=9).value
+                if tp1 is None or tp1 is not int:
+                    tp1 = 0
+                tp2 = f.cell(row=horaires, column=15).value
+                if tp2 is None:
+                    tp2 = 0
+                ares1 = f.cell(row=horaires, column=10).value
+                if ares1 is None:
+                    ares1 = 0
+                ares2 = f.cell(row=horaires, column=16).value
+                if ares2 is None:
+                    ares2 = 0
+                total = f.cell(row=horaires, column=18).value
+                if total is None:
+                    total = 0
+
+
+                feuille.cell(row=ligneH + 1, column=1, value=f.title)
+                feuille.cell(row=ligneH + 1, column=3, value=cm1+cm2)
+                feuille.cell(row=ligneH + 1, column=4, value=(td1+td2))
+                feuille.cell(row=ligneH + 1, column=5, value=(tpd1+tpd2))
+                feuille.cell(row=ligneH + 1, column=6, value=(tp1+tp2))
+                feuille.cell(row=ligneH + 1, column=7, value=(ares1+ares2))
+                feuille.cell(row=ligneH + 1, column=8, value=total)
+
+                # On insère l'année
+                cellule_selectionnee = f.cell(row=horaires, column=3).value
+                feuille.cell(row=ligneH + 1, column=2, value=cellule_selectionnee)
 
 
 

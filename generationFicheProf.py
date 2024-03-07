e27d18d5aafb6e54deea1efdb811e22f2dd2a267
MailysGodinaudG21207635 <mailys.godinaud@etu.univ-amu.fr>
2024-03-07 09:57:45 +0100

    fichier bien généré et fonctionnel

--- generationFicheProf.py
+++ generationFicheProf.py
@@ -11,7 +11,11 @@ class GenerationFicheProf:
         # Selectionner la feuille active
         ancienne_feuille = classeur['Exemple']
 
+        recherche = load_workbook("Ressources.xlsx")
+        f = recherche.active
+
         def ficheProf(prof):
+            print(prof)
 
             # Créer une nouvelle feuille pour chaque professeur
             feuille = classeur.copy_worksheet(ancienne_feuille)
@@ -21,41 +25,61 @@ class GenerationFicheProf:
             cellule_selectionnee = feuille['B1']
             cellule_selectionnee.value = prof
 
-
-            recherche = load_workbook("Ressources.xlsx")
-            f = recherche.active
             
             for f in recherche:
-                print(f.title)
-                nomprof = trouver_ligne(f, prof)
+                nomprof = trouver_ligne(f, prof, 1)
                 if nomprof is None:
                     continue
-                print(nomprof)
                 # On cherche si le professeur est responsable de la matière
                 respounon = f.cell(row=nomprof - 1, column=1).value
-                print(respounon)
 
                 # Si oui on la place responsable
                 if respounon == "Intervenants":
-                    f.insert_rows(4)
-                    f.cell(row=4, column=1, value=f.title)
+                    feuille.insert_rows(4)
+                    feuille.cell(row=4, column=1, value=f.title)
                 # Sinon on la place intervenant
                 else:
-                    rowInter = trouver_ligne(f, "Interventions")
-                    f.insert_rows(rowInter + 1)
-                    f.cell(row=rowInter + 1, column=1, value=f.title)
+                    rowInter = trouver_ligne(feuille, "Interventions", 1)
+                    feuille.insert_rows(rowInter + 1)
+                    feuille.cell(row=rowInter + 1, column=1, value=f.title)
+
+                # On récupère les volumes horaires pour chaque ressource
+                horaires = trouver_ligne(f, prof, 11)
+                if horaires is None:
+                    continue
+                ligneH = trouver_ligne(feuille, "Matieres", 1)
+                feuille.insert_rows(ligneH + 1)
+
+                for i in range(6, 10):
+                    cellule_selectionnee = f.cell(row=horaires, column=i).value
+                    if cellule_selectionnee is None:
+                        cellule_selectionnee = 0
+                    feuille.cell(row=ligneH + 1, column=1, value=f.title)
+                    feuille.cell(row=ligneH + 1, column=i-4, value=cellule_selectionnee)
 
 
 
 
-        def trouver_ligne(feuille, contenu_cible):
+
+        def trouver_ligne(feuille, contenu_cible, debut):
             for row_number, row in enumerate(feuille.iter_rows(values_only=True), start=1):
-                if contenu_cible in row:
+                if row_number >= debut and contenu_cible in row:
                     return row_number
 
             return None
 
-        ficheProf("Alain Casali")
+        liste_prof = []
+        for p in recherche:
+            for i in range(7, p.max_row):
+                prof = p.cell(row=i, column=1).value
+                if prof is None:
+                    break
+                if prof in liste_prof:
+                    continue
+                else:
+                    liste_prof.append(prof)
+                    ficheProf(prof)
+
         classeur.save("FicheProf.xlsx")
 
 def main():
